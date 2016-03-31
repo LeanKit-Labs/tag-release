b7bf30c89c2ec660614a038596ec01d2b351a453
Doug Neiner <doug.neiner@leankit.com>
2016-03-31 09:31:25 -0500

    Update to allow babel-register to run in the global npm context

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 #!/usr/bin/env node --harmony
+var path = require( "path" );
+var dir = path.basename( __dirname ); // directory this file is in
 
-require( "babel-register" );
+require( "babel-register", {
+	ignore: false,
+	only: new RegExp( path.join( dir, "src" ) )
+} );
 require( "./src/index.js" );
