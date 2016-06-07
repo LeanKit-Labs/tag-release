fc9c59c1683ef2a263be869fd2047f3de117f795
Elijah Manor <elijah.manor@gmail.com>
2016-06-07 17:59:10 -0500

    Add unit tests for auto-develop check

---
+++ test/sequence-steps/git-branch-grep-upstream-develop.js
@@ -0,0 +1,43 @@
+import test from "ava";
+import sinon from "sinon";
+import { git } from "../helpers/index.js";
+
+const utils = {
+	exec: sinon.spy( command => new Promise( resolve => resolve() ) )
+};
+
+import { gitBranchGrepUpstreamDevelop, __RewireAPI__ as RewireAPI } from "../../src/sequence-steps";
+
+test.beforeEach( t => {
+	utils.exec.reset();
+	RewireAPI.__Rewire__( "utils", utils );
+} );
+
+test.afterEach( t => {
+	RewireAPI.__ResetDependency__( "utils" );
+} );
+
+test.cb( "gitBranchGrepUpstreamDevelop calls git branch command", t => {
+	gitBranchGrepUpstreamDevelop( [ git, {} ] ).then( () => {
+		t.ok( utils.exec.calledWith( `git branch -r | grep "upstream/develop"` ) );
+		t.end();
+	} );
+} );
+
+test.cb( "gitBranchGrepUpstreamDevelop sets options.develop=true when upstream/develop exists", t => {
+	const options = { develop: false };
+	gitBranchGrepUpstreamDevelop( [ git, options ] ).then( () => {
+		t.ok( options.develop );
+		t.end();
+	} );
+} );
+
+test.cb( "gitBranchGrepUpstreamDevelop sets options.develop=false when upstream/develop does not exist", t => {
+	const options = { develop: true };
+	utils.exec = sinon.spy( command => new Promise( ( resolve, reject ) => reject() ) );
+	gitBranchGrepUpstreamDevelop( [ git, options ] ).then( () => {
+		t.ok( !options.develop );
+		t.end();
+	} );
+} );
+
