140e026fad4e3c26c2a254d88feec42fcafb8217
Elijah Manor <elijah.manor@gmail.com>
2016-08-02 09:52:20 -0500

    Help the user along with messages

--- src/sequence-steps.js
+++ src/sequence-steps.js
@@ -35,7 +35,12 @@ const sequenceSteps = [
 export function gitFetchUpstreamMaster( [ git, options ] ) {
 	const command = "git fetch upstream --tags";
 	utils.log.begin( command );
-	return utils.exec( command ).then( () => utils.log.end() );
+	return utils.exec( command )
+		.then( () => utils.log.end() )
+		.catch( () => {
+			utils.advise( "gitFetchUpstreamMaster" );
+			process.exit( 0 ); // eslint-disable-line no-process-exit
+		} );
 }
 
 export function gitBranchGrepUpstreamDevelop( [ git, options ] ) {
@@ -60,7 +65,11 @@ export function gitMergeUpstreamMaster( [ git, options ] ) {
 	const command = "git merge --ff-only upstream/master";
 	utils.log.begin( command );
 	return nodefn.lift( ::git.merge )( [ "--ff-only", "upstream/master" ] )
-		.then( () => utils.log.end() );
+		.then( () => utils.log.end() )
+		.catch( () => {
+			utils.advise( "gitMergeUpstreamMaster" );
+			process.exit( 0 ); // eslint-disable-line no-process-exit
+		} );
 }
 
 export function gitMergeUpstreamDevelop( [ git, options ] ) {
@@ -74,7 +83,13 @@ export function gitMergeUpstreamDevelop( [ git, options ] ) {
 }
 
 export function updateVersion( [ git, options ] ) {
-	const packageJson = utils.readJSONFile( "./package.json" );
+	let packageJson = {};
+	try {
+		packageJson = utils.readJSONFile( "./package.json" );
+	} catch ( e ) {
+		utils.advise( "updateVersion" );
+		process.exit( 0 ); // eslint-disable-line no-process-exit
+	}
 	const oldVersion = packageJson.version;
 	const newVersion = packageJson.version = semver.inc( oldVersion, options.release );
 	utils.writeJSONFile( "./package.json", packageJson );
@@ -103,6 +118,9 @@ export function gitLog( [ git, options ] ) {
 			utils.log.begin( command );
 			return utils.exec( command ).then( data => {
 				data = data.trim().replace( /^(.+)$/gm, "* $1" );
+				if ( data.length === 0 ) {
+					utils.advise( "gitLog" );
+				}
 				options.log = data;
 				utils.log.end();
 			} );
@@ -212,7 +230,11 @@ export function npmPublish( [ git, options ] ) {
 			} ] ).then( answers => {
 				if ( answers.publish ) {
 					utils.log.begin( command );
-					return utils.exec( command ).then( data => utils.log.end() );
+					return utils.exec( command )
+						.then( data => utils.log.end() )
+						.catch( () => {
+							utils.advise( "npmPublish" );
+						} );
 				}
 			} );
 		} ).catch( e => logger.log( chalk.red( e ) ) );
@@ -224,7 +246,10 @@ export function gitCheckoutDevelop( [ git, options ] ) {
 	if ( options.develop ) {
 		utils.log.begin( command );
 		return nodefn.lift( ::git.checkout )( "develop" )
-			.then( () => utils.log.end() );
+			.then( () => utils.log.end() )
+			.catch( () => {
+				utils.advise( "gitCheckoutDevelop" );
+			} );
 	}
 	return null;
 }
@@ -234,7 +259,11 @@ export function gitMergeMaster( [ git, options ] ) {
 	if ( options.develop ) {
 		utils.log.begin( command );
 		return nodefn.lift( ::git.merge )( [ "--ff-only", "master" ] )
-			.then( () => utils.log.end() );
+			.then( () => utils.log.end() )
+			.catch( () => {
+				utils.advise( "gitMergeMaster" );
+				process.exit( 0 ); // eslint-disable-line no-process-exit
+			} );
 	}
 	return null;
 }
