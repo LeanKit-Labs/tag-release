76678552d64625d980b31cca4f81d9137bbdb37c
Robert Reaves <robert.reaves345@gmail.com>
2017-07-07 09:56:22 -0500

    Remove merge commits for rebase with promotion.

--- src/git.js
+++ src/git.js
@@ -208,7 +208,7 @@ const git = {
 	generateRebaseCommitLog( tag ) {
 		tag = tag.slice( 1, tag.lastIndexOf( "." ) ); // remove the 'v' and version of pre-release that it is eg. .0, .1, .2, etc...
 
-		const args = `log upstream/master..HEAD --pretty=format:'%h %s'`;
+		const args = `log upstream/master..HEAD --pretty=format:'%h %s' --no-merges`;
 		return git.runCommand( { args } ).then( result => {
 			let commits = result.split( "\n" );
 
