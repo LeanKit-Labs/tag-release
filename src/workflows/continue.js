5bdba8d9bd2ee4e779536d8527b00ac72c650d06
Robert Reaves <robert.reaves345@gmail.com>
2017-08-04 09:54:39 -0500

    Added auto conflict resolution for package.json with running --pr

--- src/workflows/continue.js
+++ src/workflows/continue.js
@@ -1,7 +1,8 @@
-import * as run from "./steps";
+import * as run from "./steps/index";
+import * as run2 from "./steps/conflictResolution";
 
 export default [
-	run.verifyConflictResolution,
+	run2.verifyConflictResolution,
 	run.gitStageFiles,
 	run.gitRebaseContinue,
 	run.getFeatureBranch
--- src/workflows/pr.js
+++ src/workflows/pr.js
@@ -1,4 +1,5 @@
-import * as run from "./steps";
+import * as run from "./steps/index";
+import * as run2 from "./steps/conflictResolution";
 import { createPullRequest } from "./shared";
 
 export default [
@@ -7,11 +8,44 @@ export default [
 	run.getFeatureBranch,
 	run.gitRebaseUpstreamBranch,
 	run.saveState,
-	run.gitRebaseUpstreamDevelop,
-	...createPullRequest
+	run.getReposFromBumpCommit,
+	run.verifyPackagesToPromote,
+	run.getCurrentDependencyVersions,
+	run2.gitRebaseUpstreamDevelopWithConflictFlag
 ];
 
-export const keepTheBallRolling = [
+export const prRebaseConflict = [
+	run2.resolvePackageJSONConflicts,
+	run2.verifyConflictResolution,
+	run.gitStageFiles,
+	run.gitRebaseContinue,
+	run.getFeatureBranch,
+	run.githubUpstream,
+	run.askVersions,
+	run.updateDependencies,
+	run.gitDiff,
+	run.gitAdd,
+	run.gitAmendCommitBumpMessage,
+	run.gitForcePushUpstreamFeatureBranch,
+	run.githubUpstream,
+	run.createGithubPullRequestAganistDevelop,
+	run.cleanUpTmpFiles
+];
+
+export const prRebaseSuccess = [
+	run.githubUpstream,
+	run.askVersions,
+	run.updateDependencies,
+	run.gitDiff,
+	run.gitAdd,
+	run.gitAmendCommitBumpMessage,
+	run.gitForcePushUpstreamFeatureBranch,
+	run.githubUpstream,
+	run.createGithubPullRequestAganistDevelop,
+	run.cleanUpTmpFiles
+];
+
+export const prContinue = [
 	run.getPackageScope,
 	...createPullRequest
 ];
