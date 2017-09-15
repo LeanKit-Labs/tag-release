86ea329f32b3c23962c2125853c296a9ff612b07
Robert Reaves <robert.reaves345@gmail.com>
2017-09-15 14:01:07 -0500

    PR review comments part 1

--- specs/workflows/continue.spec.js
+++ specs/workflows/continue.spec.js
@@ -1,14 +1,14 @@
 import continueWorkflow from "../../src/workflows/continue";
-import * as run from "../../src/workflows/steps/index";
-import * as run2 from "../../src/workflows/steps/conflictResolution";
+import { gitStageFiles, gitRebaseContinue, getFeatureBranch } from "../../src/workflows/steps/index";
+import { verifyConflictResolution } from "../../src/workflows/steps/conflictResolution";
 
 describe( "continue workflow", () => {
 	it( "should have all of the required steps", () => {
 		expect( continueWorkflow ).toEqual( [
-			run2.verifyConflictResolution,
-			run.gitStageFiles,
-			run.gitRebaseContinue,
-			run.getFeatureBranch
+			verifyConflictResolution,
+			gitStageFiles,
+			gitRebaseContinue,
+			getFeatureBranch
 		] );
 	} );
 } );
