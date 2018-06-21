8dd2b7f97fd1adfc649ccc433dbadb6fda6fc22c
Robert Reaves <robert.reaves345@gmail.com>
2018-06-21 10:07:55 -0500

    Updated documentation

--- bin/tag-release.js
+++ bin/tag-release.js
@@ -4,28 +4,31 @@ const pkg = require("../package.json");
 
 commander
 	.version(pkg.version)
-	.command("start", "Default tag-release flow", { isDefault: true })
-	.command("continue", "Continues the rebase process of a tag promotion.")
+	.command("start", "default", { isDefault: true, noHelp: true })
+	.command("continue", "continue from a previously conflicted state")
 	.command(
 		"config <filePath>",
-		"Path to JSON Configuration file (defaults to './package.json')",
+		"override .json configuration file path, defaults to './package.json'",
 		/^.*\.json$/
 	)
 	.command(
 		"dev",
-		"Creates a PR from origin feature branch to upstream feature branch"
+		"create a PR from origin feature branch to upstream feature branch"
 	)
 	.command(
 		"pr [scope]",
-		"Update lightning branch and create a PR to develop."
+		"update consumer project feature branch and create a PR to develop"
 	)
-	.command("prerelease [identifier]", "Create a pre-release")
+	.command("prerelease [identifier]", "create a pre-release")
 	.alias("pre")
 	.command(
 		"promote [tag]",
-		"Promote a pre-release tag to an offical release."
+		"promote a pre-release tag previously created by tag-release"
 	)
 	.alias("pro")
-	.command("qa [scope]", "Create initial upstream branch for lightning.")
-	.command("reset", "Reset repo to upstream master/develop branches.")
+	.command(
+		"qa [scope]",
+		"create initial upstream feature branch for consumer project"
+	)
+	.command("reset", "reset repo to upstream master/develop branches")
 	.parse(process.argv);
