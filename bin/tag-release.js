7e54622abce06d4edd4905c2d9518f5ffa8447f3
Robert Reaves <robert.reaves345@gmail.com>
2018-06-21 10:29:52 -0500

    Updated help

--- bin/tag-release.js
+++ bin/tag-release.js
@@ -1,9 +1,14 @@
 #!/usr/bin/env node
+/* eslint no-console: 0 */
 const commander = require("commander");
 const pkg = require("../package.json");
+const path = require("path");
+const help = require("../src/help");
+const utils = require("../src/utils.js");
 
 commander
 	.version(pkg.version)
+	.option("--verbose", "Console additional information", { noHelp: true })
 	.command("start", "default", { isDefault: true, noHelp: true })
 	.command("continue", "continue from a previously conflicted state")
 	.command(
@@ -30,5 +35,19 @@ commander
 		"qa [scope]",
 		"create initial upstream feature branch for consumer project"
 	)
-	.command("reset", "reset repo to upstream master/develop branches")
-	.parse(process.argv);
+	.command("reset", "reset repo to upstream master/develop branches");
+
+commander.on("--help", () => {
+	help(commander);
+
+	if (commander.verbose) {
+		const diagramPath = path.resolve(__dirname, "../src/workflow.txt");
+		console.log(utils.readFile(diagramPath));
+	} else {
+		console.log(
+			"  To get a flowchart included with --help add --verbose to the command"
+		);
+	}
+});
+
+commander.parse(process.argv);
--- src/help.js
+++ src/help.js
@@ -2,27 +2,6 @@ const chalk = require("chalk");
 const { hasLkScope, renderHelpContent } = require("./utils");
 
 let helpContent = `
-  Examples:
-
-    $ tag-release                           # tag and release
-    $ tag-release --config ../config.json   # specify new config
-    $ tag-release -c ./manifest.json        # specify new config shorthand
-    $ tag-release --prerelease              # prerelease branch
-    $ tag-release -p -i rc                  # prerelease branch with identifier
-    $ tag-release --reset                   # reset or initialize repo
-    $ tag-release --verbose                 # add additional logging
-    $ tag-release -v                        # add additional logging shorthand
-    $ tag-release --promote                 # promote a prerelease tag
-    $ tag-release --promote v1.1.1-my-tag.0 # promote a specific prerelease tag
-    $ tag-release --continue                # continue a rebase after conflict
-    $ tag-release --qa                      # create host repo qa branch
-    $ tag-release --qa myorg                # create host repo qa branch w/scope
-    $ tag-release --pr                      # create host repo pr into develop
-    $ tag-release --pr myorg                # create host repo pr w/scope
-    $ tag-release --dev                     # create origin branch into upstream branch pr
-    $ tag-release --help --verbose          # display workflow diagram
-    $ tag-release --maxbuffer 3000          # override stdout maxBuffer
-
   Link to README: ${chalk.yellow.underline.bold(
 		"https://github.com/LeanKit-Labs/tag-release/blob/master/README.md"
 	)}
