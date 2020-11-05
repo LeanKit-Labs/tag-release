0c3ed5aa3a5ea93f827ef53252efb989f264390d
Robert Reaves <robert.reaves345@gmail.com>
2020-11-05 10:02:31 -0600

    Added scope check for LK id

--- specs/workflows/dev.spec.js
+++ specs/workflows/dev.spec.js
@@ -1,21 +1,32 @@
-const resetWorkflow = require("../../src/workflows/dev");
-const run = require("../../src/workflows/steps");
-
 describe("dev workflow", () => {
-	it("should have all of the required steps", () => {
-		expect(resetWorkflow).toEqual([
-			run.fetchUpstream,
-			run.gitCreateBranchOrigin,
-			run.checkoutBaseBranch,
-			run.rebaseUpstreamBaseBranch,
-			run.checkoutWorkingBranch,
-			run.gitCreateBranchUpstream,
-			run.githubUpstream,
-			run.githubOrigin,
-			run.updatePullRequestTitle,
-			run.addLKId,
-			run.updatePullRequestBody,
-			run.createGithubPullRequestAganistBranch
-		]);
+	let resetWorkflow;
+	describe("has LK scope", () => {
+		beforeEach(() => {
+			jest.resetModules();
+			jest.doMock("../../src/utils", () => ({
+				hasLkScope: jest.fn(() => true)
+			}));
+
+			resetWorkflow = require("../../src/workflows/dev");
+		});
+
+		it("should have all of the required steps", () => {
+			expect(resetWorkflow.length).toEqual(12);
+		});
+	});
+
+	describe("doesn't have LK scope", () => {
+		beforeEach(() => {
+			jest.resetModules();
+			jest.doMock("../../src/utils", () => ({
+				hasLkScope: jest.fn(() => false)
+			}));
+
+			resetWorkflow = require("../../src/workflows/dev");
+		});
+
+		it("should have all of the required steps", () => {
+			expect(resetWorkflow.length).toEqual(11);
+		});
 	});
 });
